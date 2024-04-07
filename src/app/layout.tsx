c0eccf8db3b772e0f80d3380829020866c483aa9
Diawel <38136957+diawel@users.noreply.github.com>
2024-04-07 16:12:27 +0900

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
